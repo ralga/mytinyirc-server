8ffd85596bacfc6dd10a6618a0fd81b72b594957
dedrouas <marclebosse@hotmail.fr>
2020-07-02 14:54:43 +0200

    Clean Connection

--- src/com/company/Connection.java
+++ src/com/company/Connection.java
@@ -19,17 +19,76 @@ public class Connection extends Thread{
     private final InputStream in;
     private final OutputStream out;
 
-    public Connection(String ip, int port) throws IOException, UnknownHostException {
-        this.ip = ip;
-        this.port = port;
-        sock = new Socket();
-        sock.connect(new InetSocketAddress(ip, port), TIMEOUT);	//Tentative de connexion au serveur
-        out = sock.getOutputStream();
-        in = sock.getInputStream();
+    public Connection(Socket sock) throws IOException, UnknownHostException {
+        this.ip = sock.getInetAddress().getHostAddress();
+        this.port = sock.getPort();
+        this.sock = sock;
+        this.out = sock.getOutputStream();
+        this.in = sock.getInputStream();
     }
 
     @Override
     public void run() {
         super.run();
+        do {
+            read();
+        } while (!sock.isInputShutdown());	//Tant que le flux d'entrée est ouvert
+        try {
+            if (sock.isOutputShutdown()) {	//Si tous les flux sont fermés, on peut stoper la connexion
+                sock.close();
+                /*clients.remove(this);	//Suppression du client de la liste des connectés
+                for (Client c : Client.clients) {
+                    if (c.isAuthenticated) {
+                        Message.send(c, Command.INFO);
+                    }
+                }*/
+                System.out.println("### TCP # Déconnexion : " + sock.getInetAddress().getHostAddress() + " #");
+            }
+        } catch (IOException ex) {
+            System.err.println("Impossible de fermer correctement la connexion !");
+            System.exit(1);
+        }
+    }
+
+    public void closeOutput() {
+        try {
+            sock.shutdownOutput(); //On ferme UNIQUEMENT le flux de sortie (Envoie d'un FIN)
+            //quit = true;
+        } catch (IOException ex) {
+            System.err.println("Le flux de sortie est déjà fermé !");
+        }
+    }
+
+    //Lit sur le flux d'entrée, ferme celui-ci si un end of file est reçu.
+    public void read() {
+        byte[] data = new byte[SIZE];
+        try {
+            int n = in.read(data);
+            if (n != -1) {
+                //Message.receive(this, data);	//Traitement des données reçus
+                closeOutput();
+            } else {	//End of file, un FIN vient d'être envoyé par le client
+                sock.shutdownInput();	//Ferme le flux d'entrée, met fin au Thread
+                /*if (!quit) {
+                    closeOutput();
+                }*/
+            }
+        } catch (IOException ex) {
+            System.err.println("Impossible de lire les données reçus !");
+            closeOutput();
+        }
+    }
+
+    public static void newConnection(Socket soc) {
+        Connection c;
+        try {
+            c = new Connection(soc); //Instanciation
+            c.start();	//Démarrage d'un Thread par client
+            //clients.add(c);	//Ajout à la liste des clients connectés
+            System.out.println("### TCP # Connexion : " + soc.getInetAddress().getHostAddress() + " #");
+        } catch (IOException ex) {
+            System.err.println("Impossible d'instancier le nouveau client !");
+            System.exit(1);
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,48 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
+    private static final int PORT = 4157;	//Port d'écoute du serveur
+    private static final int SIZE = 1024;	    //Send/Receive suffer size
+
+    private static Main instance = null;
+    private ServerSocket serSoc;	//Socket d'écoute
+
+    private Main(){
+
+    }
+
+    private void startServer() {
+        try {
+            serSoc = new ServerSocket(PORT);	//Lancement de la socket d'écoute
+        } catch (IOException ex) {
+            System.err.println("Impossible de démarrer le serveur !");
+            System.exit(1);
+        }
+        System.out.println("Serveur prêt ! En attente de clients...");
+        //Boucle d'écoute : attente de nouveaux clients
+        while (true) {
+            try {
+                Socket soc = serSoc.accept();	//Méthode bloquante : attend un client
+                Connection.newConnection(soc);
+            } catch (IOException ex) {
+                System.err.println("Impossible de créer la socket client !");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        getInstance().startServer();
+    }
+
+    private static final Main getInstance(){
+        if(instance == null)
+            instance = new Main();
+
+        return instance;
     }
 }
